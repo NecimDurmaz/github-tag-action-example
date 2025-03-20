aef668bdec17b529c83d49238f4bc87f96ee68ee
Necim Durmaz <96620212+NecimDurmaz@users.noreply.github.com>
2025-03-20 10:09:20 +0300

    fix: remove unnecessary whitespace and text from version display in main component

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,6 @@ import { environment } from "./environments/enviroments";
     1
     <h1>Hello from {{ name }}! {{ versionNo }}</h1>
     <h5>
-        
         Version: {{ version }}
         dsda
     </h5>
