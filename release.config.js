4490bdefe1a4ffd600c5ff7c0dd5b9f5eb685c4c
Necim Durmaz <96620212+NecimDurmaz@users.noreply.github.com>
2025-03-21 09:08:49 +0300

    feat: enhance release.config.js to include changelog generation and asset uploads

--- release.config.js
+++ release.config.js
@@ -10,13 +10,19 @@ const config = {
                 message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
             },
         ],
-        "@semantic-release/github",
+        ["@semantic-release/github", {
+            "assets": ["CHANGELOG.md", "package.json"],
+            "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+        }],
         [
             "@semantic-release/npm",
             {
                 npmPublish: false,
             },
         ],
+        ["@semantic-release/changelog", {
+            "changelogFile": "./CHANGELOG.md"
+        }],
     ],
 };
 
