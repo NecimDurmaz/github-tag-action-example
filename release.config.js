8606827d0d83600c387863150208cfa14b7be1fa
Necim Durmaz <96620212+NecimDurmaz@users.noreply.github.com>
2025-03-21 08:54:05 +0300

    feat: update release.config.js to include 'dev' branch in configuration

--- release.config.js
+++ release.config.js
@@ -1,23 +1,23 @@
 const config = {
-  branches: ["main"],
-  plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    [
-      "@semantic-release/git",
-      {
-        assets: ["dist/*.js", "dist/*.js.map"],
-        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-      },
+    branches: ["main", "dev"],
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        [
+            "@semantic-release/git",
+            {
+                assets: ["dist/*.js", "dist/*.js.map"],
+                message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+            },
+        ],
+        "@semantic-release/github",
+        [
+            "@semantic-release/npm",
+            {
+                npmPublish: false,
+            },
+        ],
     ],
-    "@semantic-release/github",
-    [
-      "@semantic-release/npm",
-      {
-        npmPublish: false,
-      },
-    ],
-  ],
 };
 
 module.exports = config;
