181116c5977b069e6657936db7da63381c5047ae
Necim Durmaz <necim.durmaz@elektraweb.com>
2025-04-10 17:45:34 +0300

    feat: update semantic-release trigger to workflow_dispatch and clean up main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import {environment} from "./environments/enviroments";
 
 @Component({
     selector: "app-root",
+
+
     template: `
         <h1>Hello from {{ name }}! {{ versionNo }}</h1>
         <H2>123asd</H2>
         h3>Version: {{ version }}
-    
+
         h3>Version: {{ version }}
         h3>Version: {{ version }}
         h3>Version: {{ version }}
