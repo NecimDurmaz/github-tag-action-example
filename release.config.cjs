a46fb12ac9a32c54fd4252e2fcb8a9a4b7cd0b25
Necim Durmaz <necim.durmaz@elektraweb.com>
2025-10-24 15:37:25 +0300

    chore: comment out release configuration in release.config.cjs
    JIRA [DEV-5123]

--- release.config.cjs
+++ release.config.cjs
@@ -1,72 +1,72 @@
-module.exports = {
-    "branches": [
-        "main",
-        "master"
-    ],
-    "plugins": [
-        "@semantic-release/commit-analyzer",
-        [
-            "@semantic-release/release-notes-generator",
-            {
-                "presetConfig": {
-                    "types": [
-                        {
-                            "type": "feat",
-                            "section": "✨ Features"
-                        },
-                        {
-                            "type": "fix",
-                            "section": "🐛 Bug Fixes"
-                        },
-                        {
-                            "type": "docs",
-                            "section": "📚 Documentation",
-                            "hidden": false
-                        },
-                        {
-                            "type": "style",
-                            "section": "🎨 Styles",
-                            "hidden": false
-                        },
-                        {
-                            "type": "refactor",
-                            "section": "♻️ Refactoring",
-                            "hidden": false
-                        },
-                        {
-                            "type": "perf",
-                            "section": "⚡ Performance",
-                            "hidden": false
-                        },
-                        {
-                            "type": "test",
-                            "section": "✅ Tests",
-                            "hidden": true
-                        },
-                        {
-                            "type": "chore",
-                            "section": "🔧 Chores",
-                            "hidden": true
-                        }
-                    ]
-                },
-                "writerOpts": {
-                    "transform": [
-                        {
-                            "pattern": "/^(feat|fix|docs|style|refactor|perf|test|chore)(\\(.+\\))?!?: (.+)$/",
-                            "script": "release-notes-transformer.js"
-                        }
-                    ]
-                }
-            }
-        ],
-        [
-            "@semantic-release/changelog",
-            {
-                "changelogFile": "CHANGELOG.md"
-            }
-        ],
-        "@semantic-release/git",
-        "@semantic-release/github"
-    ]
-}
+// module.exports = {
+//     "branches": [
+//         "main",
+//         "master"
+//     ],
+//     "plugins": [
+//         "@semantic-release/commit-analyzer",
+//         [
+//             "@semantic-release/release-notes-generator",
+//             {
+//                 "presetConfig": {
+//                     "types": [
+//                         {
+//                             "type": "feat",
+//                             "section": "✨ Features"
+//                         },
+//                         {
+//                             "type": "fix",
+//                             "section": "🐛 Bug Fixes"
+//                         },
+//                         {
+//                             "type": "docs",
+//                             "section": "📚 Documentation",
+//                             "hidden": false
+//                         },
+//                         {
+//                             "type": "style",
+//                             "section": "🎨 Styles",
+//                             "hidden": false
+//                         },
+//                         {
+//                             "type": "refactor",
+//                             "section": "♻️ Refactoring",
+//                             "hidden": false
+//                         },
+//                         {
+//                             "type": "perf",
+//                             "section": "⚡ Performance",
+//                             "hidden": false
+//                         },
+//                         {
+//                             "type": "test",
+//                             "section": "✅ Tests",
+//                             "hidden": true
+//                         },
+//                         {
+//                             "type": "chore",
+//                             "section": "🔧 Chores",
+//                             "hidden": true
+//                         }
+//                     ]
+//                 },
+//                 "writerOpts": {
+//                     "transform": [
+//                         {
+//                             "pattern": "/^(feat|fix|docs|style|refactor|perf|test|chore)(\\(.+\\))?!?: (.+)$/",
+//                             "script": "release-notes-transformer.js"
+//                         }
+//                     ]
+//                 }
+//             }
+//         ],
+//         [
+//             "@semantic-release/changelog",
+//             {
+//                 "changelogFile": "CHANGELOG.md"
+//             }
+//         ],
+//         "@semantic-release/git",
+//         "@semantic-release/github"
+//     ]
+// }
